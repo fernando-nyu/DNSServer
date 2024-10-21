426effed014c507d67eabfe11fb496e1aebd237a
Fernando Gonzalez <fg2482@nyu.edu>
2024-10-21 19:35:22 -0400

    Fix code to call TXT as cast from encrypted text.

--- DNSServer.py
+++ DNSServer.py
@@ -91,9 +91,9 @@ dns_records = {
     'legitsite.com': {
         dns.rdatatype.A: '192.168.1.104',
     },
-    'nyu.edu': {
+    'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: (encrypt_with_aes(input_string, password, salt),),
+        dns.rdatatype.TXT: (encrypted_value,),
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
